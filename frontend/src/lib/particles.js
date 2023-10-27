664fcce099a49536f786222f20edcc41dc5ad179
ElrohirGT <elrohirgt@gmail.com>
2023-10-27 12:05:55 -0600

    fix: electron definition

--- frontend/src/lib/particles.js
+++ frontend/src/lib/particles.js
@@ -1,5 +1,6 @@
 import { Particle, SimulationMagnitude } from './main'
 
 export const ELECTRON = new Particle(
-    new SimulationMagnitude(1.60217663, 'Charge', 'x10^-19C')
+    new SimulationMagnitude(-1.60217663e-19, 'Charge', 'C'),
+    new SimulationMagnitude(9.1093837e-31, "Mass", "Kg")
 )
