5e8883d1a7f2239dde3da0a31d120d9615d778ae
ElrohirGT <elrohirgt@gmail.com>
2023-10-30 20:43:29 -0600

    fix: Removed not essential dependencies

--- default.nix
+++ default.nix
@@ -5,13 +5,9 @@ in
   stdenv.mkDerivation {
     name = "Dev environment shell";
     buildInputs = [
-      exa
-      fd
 	  nodejs
 	  yarn
     ];
     shellHook = ''
-      alias ls=exa
-      alias find=fd
       '';
   }
